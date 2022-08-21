86f36f020f3384ddcef073ac2b315edda3c073bd
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-21 23:50:24 +0500

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -6,7 +6,7 @@ public Bird(String birdColor, int birdNumberOfPaws, boolean birdHasFur){
     super(birdColor, birdNumberOfPaws, birdHasFur);}
    @Override
    void getDescription(){
-        System.out.println("This animal is mostly " + color + ". It has " + numberOfPaws + " " + stringNumberOfPaws(super.numberOfPaws) + " and " + booleanHasFur(hasFur) + " " + "fur." + " Moreover, it has 2 wings and can fly");
+        System.out.println("This animal is mostly " + super.color + ". It has " + super.numberOfPaws + " " + super.stringNumberOfPaws(super.numberOfPaws) + " and " + super.booleanHasFur(super.hasFur) + " " + "fur." + " Moreover, it has 2 wings and can fly");
     
  
   } 
