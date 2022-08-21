e75c3e52d67bb16426a2a34f6d9578b1947ceea6
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-21 23:31:51 +0500

    Update Dog.java

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,12 +1,12 @@
 package com.epam.mjc.stage0;
 class Dog extends Animal {
 
-  public Dog(){
-    super("brown", 4, true);
+  public Dog(String dogColor, int dogNumberOfPaws, boolean dogHasFur){
+    super(dogColor, dogNumberOfPaws, dogHasFur);
     
   }
    void getDescription(){
-        System.out.println("This animal is mostly " + super.color + ". It has " + super.numberOfPaws + " " + super.stringNumberOfPaws(super.numberOfPaws) + " and " + super.booleanHasFur(super.hasFur) + "fur.");
+        System.out.println("This animal is mostly " + color + ". It has " + numberOfPaws + " " + stringNumberOfPaws(numberOfPaws) + " and " + booleanHasFur(hasFur) + "fur.");
     }
     
   
