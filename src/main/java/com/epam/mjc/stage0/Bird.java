9c49a483e04e7fc0299c27f9d0e1f8dc2f782c92
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-22 07:24:33 +0500

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -2,15 +2,13 @@ package com.epam.mjc.stage0;
 
 class Bird extends Animal {
 
-public Bird(String birdColor, int birdNumberOfPaws, boolean birdHasFur){
-    super(birdColor, birdNumberOfPaws, birdHasFur);}
+public Bird(){
+        super("blue",2, false);
+    }
    @Override
-   void getDescription(){
-       super.getDescription();
-        System.out.print(" Moreover, it has 2 wings and can fly");
-    
- 
-  } 
+   public String getDescription(){
+    return super.getDescription()+" Moreover, it has 2 wings and can fly.";
+}
     
   
 
