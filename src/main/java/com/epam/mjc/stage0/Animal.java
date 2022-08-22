a26932c9a4e822fa75093a6cb8ede308d46527e6
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-22 07:23:04 +0500

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -9,27 +9,29 @@ class Animal{
         this.numberOfPaws = animalNumberOfPaws;
         this.hasFur = animalHasFur;
     }
+    
+    public String booleanHasFur(boolean hasFur){
+    if (hasFur == true){
+        return "a";
+    }
+    else{
+        return "no";
+    }
+ }
+    public String stringNumberOfPaws(int num){
+    if(num > 1){
+    return "paws";
+    }
+    else{
+    return "paw";
+    }
+    }
 
 
-    void getDescription(){
-        
-        System.out.print("This animal is mostly " + color + ".");
-        if (numberOfPaws > 1){
-            System.out.print(" It has " + numberOfPaws + " paws ");
-
-        }
-        else if(numberOfPaws == 1){
-           System.out.print(" It has " + numberOfPaws + " paw ");
-
-        }
-        if (hasFur){
-            System.out.print("and a fur.");
-        }
-        else{
-            System.out.print("and no fur.");
-        }
-            
-        }
+   public String  getDescription(){
+    String res  = "This animal is mostly " + color + ". It has " + numberOfPaws + " " + stringNumberOfPaws(numberOfPaws) + " and " + booleanHasFur(hasFur) + " fur.";
+    return res;
+}
             
         
     
